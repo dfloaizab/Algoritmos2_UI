031b8103dc2cb37611dea3a56074e0ecde3c72f6
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2022-05-13 19:52:18 -0500

    Create PanelTablero.java

---
+++ Triqui/PanelTablero.java
@@ -0,0 +1,15 @@
+package com.usc.UI;
+
+import javax.swing.*;
+
+public class PanelTablero extends JPanel {
+
+    JLabel etiquetaPrueba;
+
+    public PanelTablero() {
+
+        etiquetaPrueba = new JLabel("Agui va el tablero");
+        this.add(etiquetaPrueba);
+
+    }
+}

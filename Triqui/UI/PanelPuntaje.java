2dce4c2743399e73dc1cc3674dcc5b52348bd153
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2022-05-13 20:12:25 -0500

    Create PanelPuntaje.java

---
+++ Triqui/UI/PanelPuntaje.java
@@ -0,0 +1,15 @@
+package com.usc.UI;
+
+import javax.swing.*;
+
+public class PanelPuntaje extends JPanel{
+
+    JLabel etiquetaPuntaje;
+
+    public PanelPuntaje() {
+
+        etiquetaPuntaje = new JLabel("Aqui va el puntaje");
+        add(etiquetaPuntaje);
+
+    }
+}

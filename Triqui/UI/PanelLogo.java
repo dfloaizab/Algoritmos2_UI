0d75480ed4ed5a50063035c5ca40595c30a461e6
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2022-05-13 20:11:26 -0500

    Create PanelLogo.java

---
+++ Triqui/UI/PanelLogo.java
@@ -0,0 +1,14 @@
+package com.usc.UI;
+
+import javax.swing.*;
+
+public class PanelLogo extends JPanel {
+
+    JLabel textoLogo;
+
+    public PanelLogo() {
+
+        textoLogo = new JLabel("Aqui va el logo");
+        add(textoLogo);
+    }
+}
